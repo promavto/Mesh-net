520df62208072c888f2630417cb0e67d0275bbe9
promavto <promavto@ntmp.ru>
2021-08-25 13:01:43 +0300

    fix

--- TestSoft/ESP32_Timer_Test_Serial/ESP32_Timer_Test_Serial.ino
+++ TestSoft/ESP32_Timer_Test_Serial/ESP32_Timer_Test_Serial.ino
@@ -1,10 +1,10 @@
 /*
- Repeat timer example
- This example shows how to use hardware timer in ESP32. The timer calls onTimer
- function every second. The timer can be stopped with button attached to PIN 0
- (IO0).
- This example code is in the public domain.
- */
+  Repeat timer example
+  This example shows how to use hardware timer in ESP32. The timer calls onTimer
+  function every second. The timer can be stopped with button attached to PIN 0
+  (IO0).
+  This example code is in the public domain.
+*/
 
 // Stop button is attached to PIN 0 (IO0)
 #define BTN_STOP_ALARM    32
@@ -15,11 +15,12 @@ portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 volatile uint32_t isrCounter = 0;
 volatile uint32_t lastIsrAt = 0;
+//volatile uint32_t timer_autoreload_t = TIMER_AUTORELOAD_DIS;
 
 void IRAM_ATTR onTimer() {
-//  portENTER_CRITICAL_ISR(&timerMux);
-//  interruptCounter++;
-//  portEXIT_CRITICAL_ISR(&timerMux);
+  //  portENTER_CRITICAL_ISR(&timerMux);
+  //  interruptCounter++;
+  //  portEXIT_CRITICAL_ISR(&timerMux);
 
   // Increment the counter and set the time of ISR
   portENTER_CRITICAL_ISR(&timerMux);
@@ -29,11 +30,11 @@ void IRAM_ATTR onTimer() {
   // Give a semaphore that we can check in the loop
   xSemaphoreGiveFromISR(timerSemaphore, NULL);
   // It is safe to use digitalRead/Write here if you want to toggle an output
- 
+
 }
 
 
-
+bool button_on = false;
 
 //void ARDUINO_ISR_ATTR onTimer()
 //{
@@ -72,9 +73,11 @@ void setup() {
   timerAlarmEnable(timer);
 }
 
-void loop() {
+void loop()
+{
   // If Timer has fired
-  if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE){
+  if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE)
+  {
     uint32_t isrCount = 0, isrTime = 0;
     // Read the interrupt count and time
     portENTER_CRITICAL(&timerMux);
@@ -89,12 +92,31 @@ void loop() {
     Serial.println(" ms");
   }
   // If button is pressed
-  if (digitalRead(BTN_STOP_ALARM) == LOW) {
+  if (digitalRead(BTN_STOP_ALARM) == LOW)
+  {
     // If timer is still running
-    if (timer) {
-      // Stop and free timer
-      timerEnd(timer);
-      timer = NULL;
+    button_on = !button_on;
+    if (button_on)
+    {
+      if (timer)
+      {
+        // Stop and free timer
+        timerAlarmEnable(timer);
+        timerEnd(timer);
+        timer = NULL;
+      }
     }
+    if (!button_on)
+    {
+      if (timer)
+      {
+        // Stop and free timer
+        timerAlarmEnable(timer);
+        timerOn(timer);
+        timer = NULL;
+      }
+    }
+   
+    
   }
 }
